e0e269e93dc1ccdd91ca05ab05fbbb545d3862ed
Yudai-creator <perezheber73@gmail.com>
2022-01-17 17:42:57 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Hello mate</h1>
+  </div>
+  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
