6fecb04f9cd2c488c0d68ee0e51dc77246fb755b
Yudai-creator <perezheber73@gmail.com>
2022-03-14 15:09:07 -0400

    start coding hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="container">
 
-    <div ref="texts" class="text">
-      <p ref="intro" class="anim">Hello dear visitant 🖐🏻</p>
-      <p ref="yudai" class="anim">Yudai here!</p>
-      <p ref="warning" class="anim">This site is currently under constrution <span ref="building">🚧</span></p>
-    </div>
+  <!--Hero section-->
+  <div class="hero_section">
+    <header>
+      <figure>
+        <img class="logo" src="~/static/imgs/yudai.png" alt="yudai_logo">
+      </figure>
 
-    <div ref="brand" class="brand-wrapper">
-      <img src="~/static/imgs/yudai-creator.png">
-      <p ref="social">You can follow me on <a href="https://twitter.com/creator_yudai" target="_blank">twitter</a> <br>
-      to know more about the buliding process</p>
-    </div>
-
-    <!--
       
-    -->
-    
+      <svg class="fries_menu" xmlns="http://www.w3.org/2000/svg" width="39" height="17" viewBox="0 0 39 17">
+        <g id="fries_menu" data-name="fries menu" transform="translate(-3362 30)">
+          <rect id="Rectángulo_101" data-name="Rectángulo 101" width="39" height="3" rx="1.5" transform="translate(3362 -16)" fill="#00a1ff"/>
+          <rect id="Rectángulo_148" data-name="Rectángulo 148" width="27" height="3" rx="1.5" transform="translate(3374 -23)" fill="#00a1ff"/>
+          <rect id="Rectángulo_102" data-name="Rectángulo 102" width="39" height="3" rx="1.5" transform="translate(3362 -30)" fill="#00a1ff"/>
+        </g>
+      </svg>
+      
+
+      
+      <img class="blob-right-corner" src="~/static/landing_assets/blob shape right corner.svg" alt="blob_shape">
+      <img class="blob-stroke" src="~/static/landing_assets/blob stroke 1.svg" alt="stroke">
+      <img class="blob-right-corner-shadow" src="~/static/landing_assets/blob shape right corner shadow.svg" alt="blob_shape">
+      
+      
+
+    </header> 
   </div>
   
 </template>
@@ -43,67 +51,21 @@ export default {
 </script>
 
 <style lang="scss" >
+@use '~/assets/variables' as *;
+@import '~/assets/styles/landing_section';
 
-body{
+// global styles
+*{
+  box-sizing: border-box;
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
-  overflow: hidden;
-
-  background-color: #e5e5e5;
 }
 
-.container{
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  .text{
-    position: relative;
-    width: 90%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
-    p{
-      //background-color: gray;
-      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-      font-weight: 600;
-      padding: 1rem;
-      font-size: 2rem;
-      color: #010c22;
-    }
-
-    .anim{
-      transform-origin: center;
-      transform: scale(0);
-    }
-  }
-
-  .brand-wrapper{
-    position: absolute;
-    opacity: 0;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img{
-      width: 50%;
-    }
-
-    p{
-      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-      font-weight: 600;
-      color: #010c22;
-      font-size: 1.2rem;
-
-      a{
-        color: rgb(77, 207, 207);
-      }
-    }
-  }
-  
+body {
+    background-color: $bg-color;
+    overflow-x: hidden;
 }
+
 
 
 @media (max-width: 480px){
