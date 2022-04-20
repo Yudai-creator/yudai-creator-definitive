451af4fc6a33287e0e02a05fc91bc46c37be37ed
Yudai-creator <perezheber73@gmail.com>
2022-04-20 15:31:51 -0400

    fix: restart due to errors with css

--- pages/index.vue
+++ pages/index.vue
@@ -8,20 +8,21 @@
           <img class="logo" src="~/static/imgs/yudai.png" alt="yudai_logo">
         </figure>
 
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50" width="50px" height="50px">
+        <svg class="header_hamburger-menu" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50" width="50px" height="50px">
           <g id="surface1032491">
             <path style=" stroke:none;fill-rule:nonzero;fill:rgb(20.392157%,59.607846%,85.882354%);fill-opacity:1;" d="M 5 8 C 4.277344 7.988281 3.609375 8.367188 3.246094 8.992188 C 2.878906 9.613281 2.878906 10.386719 3.246094 11.007812 C 3.609375 11.632812 4.277344 12.011719 5 12 L 45 12 C 45.722656 12.011719 46.390625 11.632812 46.753906 11.007812 C 47.121094 10.386719 47.121094 9.613281 46.753906 8.992188 C 46.390625 8.367188 45.722656 7.988281 45 8 Z M 5 23 C 4.277344 22.988281 3.609375 23.367188 3.246094 23.992188 C 2.878906 24.613281 2.878906 25.386719 3.246094 26.007812 C 3.609375 26.632812 4.277344 27.011719 5 27 L 45 27 C 45.722656 27.011719 46.390625 26.632812 46.753906 26.007812 C 47.121094 25.386719 47.121094 24.613281 46.753906 23.992188 C 46.390625 23.367188 45.722656 22.988281 45 23 Z M 5 38 C 4.277344 37.988281 3.609375 38.367188 3.246094 38.992188 C 2.878906 39.613281 2.878906 40.386719 3.246094 41.007812 C 3.609375 41.632812 4.277344 42.011719 5 42 L 45 42 C 45.722656 42.011719 46.390625 41.632812 46.753906 41.007812 C 47.121094 40.386719 47.121094 39.613281 46.753906 38.992188 C 46.390625 38.367188 45.722656 37.988281 45 38 Z M 5 38 "/>
           </g>
         </svg>
 
-          
-
-          
-        <img id="blob-right-corner" src="~/static/landing_assets/blob shape right corner.svg" alt="blob_shape">
-        <img id="blob-stroke" src="~/static/landing_assets/blob stroke 1.svg" alt="stroke">
+        
+ 
+        <!-- <img id="blob-right-corner" src="~/static/landing_assets/blob shape right corner.svg" alt="blob_shape"> -->
+        <!-- <img id="blob-right-corner" src="~/static/landing_assets/blob-shape-right-corner.png" alt="blob_shape"> -->
+        <!-- <img id="blob-stroke" src="~/static/landing_assets/blob stroke 1.svg" alt="stroke">
         <img id="blue_cross" src="~/static/landing_assets/blue cross.svg" alt="cross">
-        <img id="blue_cross2" src="~/static/landing_assets/blue cross2.svg" alt="cross">
-        <img id="blob-right-corner-shadow" src="~/static/landing_assets/blob shape right corner shadow.svg" alt="blob_shape">
+        <img id="blue_cross2" src="~/static/landing_assets/blue cross2.svg" alt="cross"> -->
+        <!-- <img id="blob-right-corner-shadow" src="~/static/landing_assets/blob-shape-right-corner-shadow.png" alt="blob_shape"> -->
+
         
       </header> 
 
@@ -45,14 +46,14 @@
     </div>
 
     <!--About me section-->
-    <div class="about_blobs">
+    <!-- <div class="about_blobs">
       <img id="blob-left-corner" src="~/static/about-me_assets/blob-shape-left-corner.svg" alt="blob_shape">
       <img id="blob-left-corner-shadow" src="~/static/about-me_assets/blob-shape-left-corner-shadow.svg" alt="blob_shape">
-    </div>
-    <div class="about_section">
+    </div> -->
+    <!-- <div class="about_section">
       <img class="about_profile-picture" src="~/static/imgs/yudai-pfp-shadow.png" alt="profile-pic">
       <div class="about_text">
-        <p >
+        <p>
           Hello dear visitant, Yudai here🖐
           I'm a <span>web developer</span> and <span>UI designer</span>.
         </p>
@@ -72,6 +73,17 @@
           and contribute with the society.
         </p>
       </div>
+    </div> -->
+    <!-- Social media spot -->
+    <div class="social-media_section">
+      <img id="wave-top" src="~/static/social-media_assets/waves-top.png" alt="waves">
+      <div class="social-media_spot-twitter">
+        <h2>I share my everyday learnings and achievements</h2>
+        <div class="spot-twitter_banner">
+          <img id="twitter-banner" src="~/static/social-media_assets/BANNER TWITTER.png" alt="banner">
+        </div>
+      </div>
+      <img id="wave-bottom" src="~/static/social-media_assets/waves-bottom.png" alt="waves">
     </div>
     <footer>
 
@@ -111,6 +123,7 @@ export default {
 @use '~/assets/variables' as *;
 @import '~/assets/styles/landing_section';
 @import '~assets/styles/about-me_section';
+@import '~assets/styles/social-media_section';
 
 // global styles
 *{
@@ -139,6 +152,12 @@ body {
     // -webkit-box-sizing: border-box;
     // -moz-box-sizing: border-box;
     
+}
+
+footer{
+  background-color: black;
+  width: 100%;
+  height: 200px;
 }
 
 // body::-webkit-scrollbar{
