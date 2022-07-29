bb53d8b3260e57b55ab20dc491cbfc31e36b09e9
Yudai-creator <perezheber73@gmail.com>
2022-07-29 16:33:34 -0400

    meta description added

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', 
+        name: 'description', 
+        content: 'Yudai H portfolio website, showing up his skills as a designer and fronted developer' 
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
