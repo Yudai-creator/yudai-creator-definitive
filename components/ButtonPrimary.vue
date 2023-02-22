d71abdd68afc954fe4290a4ba96cee2dc017dda2
Yudai-creator <yudaipx77@gmail.com>
2023-02-22 15:40:11 -0500

    feat:animations done

--- components/ButtonPrimary.vue
+++ components/ButtonPrimary.vue
@@ -1,16 +1,24 @@
 <template name="ButtonPrimary">
-    <div>
+    <div class="cta-wrapper">
         <a class="cta-button" href="#work">{{textContent}}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m12 15.586-4.293-4.293-1.414 1.414L12 18.414l5.707-5.707-1.414-1.414z" fill="#fff"/><path d="m17.707 7.707-1.414-1.414L12 10.586 7.707 6.293 6.293 7.707 12 13.414z" fill="#fff"/></svg>
         </a>
     </div>
 </template>
+
 <script>
+import {gsap} from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 // JS
 export default  {
     props:{
         textContent: String,
+    },
+
+    mounted(){
+        gsap.to('.cta-button', {duraion: 1, 'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', y: 0, ease: "circ.out"}, .7)
     }
 }
 </script>
@@ -33,6 +41,9 @@ div{
 
 
 .cta-button {
+    clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
+    transform: translateY(10px);
+
     z-index: 2;
     display: flex;
     width: 200px;
